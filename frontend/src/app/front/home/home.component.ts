8f66b2d28339751aec25a412d752b76335cec023
Mark Figues <markcarlofigues.digima@gmail.com>
2018-08-17 14:47:23 +0800

    updated front

--- frontend/src/app/front/home/home.component.ts
+++ frontend/src/app/front/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,19 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	this.smooth_scroll();
+  }
+
+  smooth_scroll(): void
+  {
+    $(document).on('click', 'a.navigation__link', function(e){
+      e.preventDefault();
+      var link = $(this).attr('href');
+
+      $('html, body').animate({
+        scrollTop: $(link).offset().top - 65}, 800 );
+      return false; 
+    });
   }
 
 }
