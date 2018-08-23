ee8a50ae3ee79ac5b6962c005c4137d49ada2cab
Rocky Celis <rckycls000@gmail.com>
2018-08-23 22:36:55 +0800

    backend to frontend

--- frontend/src/app/member/member-info.service.ts
+++ frontend/src/app/member/member-info.service.ts
@@ -59,7 +59,7 @@ export class MemberInfoService
 		}
 		else
 		{
-			this.api_url 				= "https://api.successmall.io";
+			this.api_url 				= "http://api.successmall.io";
 		}
 		
 		this.first_name 				= "";
