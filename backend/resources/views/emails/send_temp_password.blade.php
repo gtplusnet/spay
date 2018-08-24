56d6c7d124f90e009ebc89a8587c6e68705aa419
Rocky Celis <rckycls000@gmail.com>
2018-08-24 15:42:15 +0800

    email

--- backend/resources/views/emails/send_temp_password.blade.php
+++ backend/resources/views/emails/send_temp_password.blade.php
@@ -6,6 +6,6 @@
 </p>
 <div class="text-center"><strong>Temporary Password:</strong> {{$passkey}}</div>
 <div style="text-align: center; padding: 15px 0px;">
-	<a href="http://successmall.io/login" target="_blank" style="padding: 12px; border: 1px solid rgba(10,0,39,1); background: rgba(10,0,39,0.8); color: #eee; cursor: pointer; text-decoration: none; font-size: 13px;">Click Here to Login</a>
+	<a href="http://successmall.io/login" target="_blank" style="padding: 12px; border: 1px solid#aa7809; background: #aa7809; color: #eee; cursor: pointer; text-decoration: none; font-size: 13px;">Click Here to Login</a>
 </div>
 @endsection
