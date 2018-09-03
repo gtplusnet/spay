696d41e1d7d902b5d137743a31957931a793e581
Rocky Celis <rckycls000@gmail.com>
2018-09-03 14:20:14 +0800

    styles

--- backend/resources/views/emails/email_layout.blade.php
+++ backend/resources/views/emails/email_layout.blade.php
@@ -7,7 +7,7 @@
 <div style="background: #12385b; width: 60%; margin: auto; border-top: 5px solid #051a2d; border-bottom: 5px solid #051a2d; padding: 20px 30px; box-shadow: 0px 0px 3px rgba(18, 56, 91,0.3); text-align: center; color: #fff; font-family: Arial, Helvetica, sans-serif;">
 	<img src="https://i.imgur.com/G3yumV9.png" width="150"  alt="">
 	<!-- <h5 style="margin-top: 0; color: #aa7809"></h5> -->
-	<div style="background: #aa7809; border-radius: 10px; padding: 20px; text-align: left;">
+	<div style="background: #051a2d; border-radius: 10px; padding: 20px; text-align: left;">
 		@yield("content")
 		<h4 style="margin-bottom: 10px;">Best Regards,</h4>
 		<img src="https://i.imgur.com/IJAtk2T.png" width="150"  alt="">
