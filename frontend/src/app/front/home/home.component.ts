b3c62693d52a2326b298351dda774a4e1cf46821
carlosegovia <carlosegovia5@gmail.com>
2018-09-03 17:03:18 +0800

    ahm mob res

--- frontend/src/app/front/home/home.component.ts
+++ frontend/src/app/front/home/home.component.ts
@@ -23,42 +23,49 @@ export class HomeComponent implements OnInit {
 
 	icoNewsSwiper(): void
 	{
-		const swiper = new Swiper('.swiper-ico-news',
-		{
-			slidesPerView: 3,
-			autoplay:
-			{
-				delay: 2500,
-				disableOnInteraction: false
-			},
-			spaceBetween: 10,
-
-			breakpoints: {
-				1024: {
-					slidesPerView: 4,
-					spaceBetween: 40
-				},
-				768: {
-					slidesPerView: 3,
-					spaceBetween: 30
-				},
-				640: {
-					slidesPerView: 2,
-					spaceBetween: 20
-				},
-				320: {
-					slidesPerView: 1,
-					spaceBetween: 10
-				}
-			},
-
-			// Navigation arrows
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			},
-
-		});
+		var swiper = new Swiper('.swiper-ico-news', {
+	    slidesPerView: 3,
+	    spaceBetween: 10,
+	    loop: true,
+	    loopFillGroupWithBlank: true,
+	    autoplay:
+	    {
+	      	delay: 2500,
+	      	disableOnInteraction: false
+	    },
+	    breakpoints: 
+	    {
+	      	1024: 
+	      	{
+	      		slidesPerView: 3,
+	      		spaceBetween: 40
+	      	},
+	      	768: 
+	      	{
+	      		slidesPerView: 2,
+	      		spaceBetween: 30
+	      	},
+	      	640: 
+	      	{
+	      		slidesPerView: 1,
+	      		spaceBetween: 20
+	      	},
+	      	320: 
+	      	{
+	      		slidesPerView: 1,
+	      		spaceBetween: 10
+	      	}
+	    },
+	    pagination: {
+	            el: '.swiper-pagination',
+	            clickable: true,
+	          },
+	    navigation: 
+	    {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+	    });
 	}
 	smooth_scroll(): void
 	{
