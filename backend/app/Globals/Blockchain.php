19e3635229c82537dc4af7dc5fd55a19e6c6d6d9
Rocky Celis <rckycls000@gmail.com>
2018-10-12 18:06:31 +0800

    asdasd

--- backend/app/Globals/Blockchain.php
+++ backend/app/Globals/Blockchain.php
@@ -811,6 +811,7 @@ class Blockchain
 
     public static function eth_send_transaction($params)
     {
+        dd($params, $params["tx"]);
         $api_code = "7e7ea4a09e96460b8b20c915a48bcfb6";
 
         $url = "https://api.blockcypher.com/v1/eth/main/txs/send?token=".$api_code;
