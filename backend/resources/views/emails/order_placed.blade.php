1243312a0c1f13a3bf93ddc0a3b6131bb63e1d6d
Rocky Celis <rckycls000@gmail.com>
2018-10-17 11:33:28 +0800

    mails

--- backend/resources/views/emails/order_placed.blade.php
+++ backend/resources/views/emails/order_placed.blade.php
@@ -5,14 +5,17 @@
 <p style="text-indent: 50px;">
 	You have recently placed an order of <strong>{{ $amount }} XS Tokens</strong> via {{ $method }}. Please pay on or before {{ date("F j, Y",strtotime($record["expiration_date"])) }}. Failing to pay the expected payment on time will result to order expiration. Thank you!
 </p>
-<hr style="margin: 3px 0px">
-<p style="text-align: center;">
-	<h4 style="color: #555">Copy this wallet address: </h4>
-	<h5>{{ $address["member_address"] }}</h5>
-	<h6 style="margin: 3px 0px">OR</h6>
-	<h4>Scan this QR Code: </h4>
-	<img src="http://chart.apis.google.com/chart?cht=qr&chs=200x200&chl={{ $address["member_address"] }}&chld=H|0">
-</p>
+<hr style="margin: 3px 0px; border-color: rgba(255,255,255,0.3);">
+<div style="text-align: center !important;">
+	<p style="text-align: center;">
+		<h4 style="color: #ddd">Copy this wallet address: </h4>
+		<h3>{{ $address["member_address"] }}</h3>
+		<h6 style="margin: 3px 0px">OR</h6>
+		<h4 style="color: #ddd">Scan this QR Code: </h4>
+		<img src="http://chart.apis.google.com/chart?cht=qr&chs=200x200&chl={{ $address["member_address"] }}&chld=H|0">
+	</p>
+</div>
+
 {{-- <small><em>
 	To check your transactions details. Go to www.successmall.io, <strong>Login</strong> your account and in member dashboard click <strong>BTC or ETH Transactions</strong>.
 </em></small> --}}
