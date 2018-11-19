4f863246471c4391de2675e2241dae733f560817
Rocky Celis <rckycls000@gmail.com>
2018-11-19 16:02:00 +0800

    emails

--- backend/resources/views/emails/email_layout.blade.php
+++ backend/resources/views/emails/email_layout.blade.php
@@ -5,12 +5,12 @@
 </head>
 <body style="padding: 10px; margin: auto;">
 <div style="background: #f4d56b; width: 70%; margin: auto; border-top: 5px solid #aa7809; border-bottom: 5px solid #aa7809; padding: 20px 30px; box-shadow: 0px 0px 3px rgba(10,0,39,0.3); text-align: center; color: #fff; font-family: Arial, Helvetica, sans-serif;">
-	<img src="https://i.imgur.com/w4JbDe9.png" width="150"  alt="">
+	<img src="" width="150"  alt="">
 	<h5 style="margin-top: 0; color: #aa7809">Giving Cryptocurrencies Life</h5>
 	<div style="background: #aa7809; border-radius: 10px; padding: 20px; text-align: left;">
 		<!-- @yield("content") -->
 		<h4 style="margin-bottom: 10px;">Best Regards,</h4>
-		<img src="https://i.imgur.com/4GrEPmv.png" width="150"  alt="">
+		<img src="" width="150"  alt="">
 	</div>
 	<!-- <div style="margin-top: 20px; font-size: 13px;">
 		<a href="https://successpay.io/" target="_blank" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">Homepage</a> |
