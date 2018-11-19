a11fec348664f3fb13bad1f158f28962ab997aa8
Rocky Celis <rckycls000@gmail.com>
2018-11-19 15:53:10 +0800

    emails

--- backend/resources/views/emails/email_layout.blade.php
+++ backend/resources/views/emails/email_layout.blade.php
@@ -4,11 +4,11 @@
 	<title></title>
 </head>
 <body style="padding: 10px; margin: auto;">
-<!-- <div style="background: #f4d56b; width: 70%; margin: auto; border-top: 5px solid #aa7809; border-bottom: 5px solid #aa7809; padding: 20px 30px; box-shadow: 0px 0px 3px rgba(10,0,39,0.3); text-align: center; color: #fff; font-family: Arial, Helvetica, sans-serif;">
+<div style="background: #f4d56b; width: 70%; margin: auto; border-top: 5px solid #aa7809; border-bottom: 5px solid #aa7809; padding: 20px 30px; box-shadow: 0px 0px 3px rgba(10,0,39,0.3); text-align: center; color: #fff; font-family: Arial, Helvetica, sans-serif;">
 	<img src="https://i.imgur.com/w4JbDe9.png" width="150"  alt="">
 	<h5 style="margin-top: 0; color: #aa7809">Giving Cryptocurrencies Life</h5>
 	<div style="background: #aa7809; border-radius: 10px; padding: 20px; text-align: left;">
-		@yield("content")
+		<!-- @yield("content") -->
 		<h4 style="margin-bottom: 10px;">Best Regards,</h4>
 		<img src="https://i.imgur.com/4GrEPmv.png" width="150"  alt="">
 	</div>
@@ -18,7 +18,6 @@
 		<a href="#" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">White Paper</a> |
 		<a href="#" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">Community</a>
 	</div>
-</div> -->
-i was here
+</div>
 </body>
 </html>
