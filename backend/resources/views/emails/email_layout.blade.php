8ee43c5ca8c6ed6b96c70983dbccaa6e504d1baa
Rocky Celis <rckycls000@gmail.com>
2018-11-19 16:12:20 +0800

    emails

--- backend/resources/views/emails/email_layout.blade.php
+++ backend/resources/views/emails/email_layout.blade.php
@@ -5,19 +5,19 @@
 </head>
 <body style="padding: 10px; margin: auto;">
 <div style="background: #f4d56b; width: 70%; margin: auto; border-top: 5px solid #aa7809; border-bottom: 5px solid #aa7809; padding: 20px 30px; box-shadow: 0px 0px 3px rgba(10,0,39,0.3); text-align: center; color: #fff; font-family: Arial, Helvetica, sans-serif;">
-	<img src="" width="150"  alt="">
+	<img src="https://successpay.io/assets/img/email_header.png" width="150"  alt="">
 	<h5 style="margin-top: 0; color: #aa7809">Giving Cryptocurrencies Life</h5>
 	<div style="background: #aa7809; border-radius: 10px; padding: 20px; text-align: left;">
-		<!-- @yield("content") -->
+		@yield("content")
 		<h4 style="margin-bottom: 10px;">Best Regards,</h4>
-		<img src="" width="150"  alt="">
+		<img src="https://successpay.io/assets/img/email_footer.png" width="150"  alt="">
 	</div>
-	<!-- <div style="margin-top: 20px; font-size: 13px;">
+	<div style="margin-top: 20px; font-size: 13px;">
 		<a href="https://successpay.io/" target="_blank" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">Homepage</a> |
 		<a href="https://successpay.io/register" target="_blank" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">Sign Up</a> |
 		<a href="#" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">White Paper</a> |
 		<a href="#" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">Community</a>
-	</div> -->
+	</div>
 </div>
 </body>
 </html>
