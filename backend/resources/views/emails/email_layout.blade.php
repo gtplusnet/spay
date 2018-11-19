56f4b804d2082c20d31ed58c63529e2dbd0dd3c1
Rocky Celis <rckycls000@gmail.com>
2018-11-19 15:24:44 +0800

    emails

--- backend/resources/views/emails/email_layout.blade.php
+++ backend/resources/views/emails/email_layout.blade.php
@@ -4,7 +4,7 @@
 	<title></title>
 </head>
 <body style="padding: 10px; margin: auto;">
-<div style="background: #f4d56b; width: 70%; margin: auto; border-top: 5px solid #aa7809; border-bottom: 5px solid #aa7809; padding: 20px 30px; box-shadow: 0px 0px 3px rgba(10,0,39,0.3); text-align: center; color: #fff; font-family: Arial, Helvetica, sans-serif;">
+<!-- <div style="background: #f4d56b; width: 70%; margin: auto; border-top: 5px solid #aa7809; border-bottom: 5px solid #aa7809; padding: 20px 30px; box-shadow: 0px 0px 3px rgba(10,0,39,0.3); text-align: center; color: #fff; font-family: Arial, Helvetica, sans-serif;">
 	<img src="https://i.imgur.com/w4JbDe9.png" width="150"  alt="">
 	<h5 style="margin-top: 0; color: #aa7809">Giving Cryptocurrencies Life</h5>
 	<div style="background: #aa7809; border-radius: 10px; padding: 20px; text-align: left;">
@@ -18,6 +18,7 @@
 		<a href="#" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">White Paper</a> |
 		<a href="#" style="text-decoration: none; color: #aa7809; margin: 0px 5px;">Community</a>
 	</div>
-</div>
+</div> -->
+i was here
 </body>
 </html>
